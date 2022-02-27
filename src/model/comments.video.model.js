f856013779cf790f730d665696e413fae96a71db
Cesar Arroyo <cesardavid_09@hotmail.com>
2022-02-27 17:42:25 -0500

    feat: Adding model comments video

---
+++ src/model/comments.video.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const commentsVideoSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  type: {
+    type: String
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  _video: { type: Schema.ObjectId, ref: 'Video' }
+  //   _student: { type: Schema.ObjectId, ref: 'Student' }, TODO AFTER MAKE STUDENT MODEL
+})
+const CommentsVideo = mongoose.model('CommentsVideo', commentsVideoSchema)
+export default CommentsVideo
